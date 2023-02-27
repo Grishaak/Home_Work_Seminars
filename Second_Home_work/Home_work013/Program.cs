fb3cb5c3577a361dac421382d24caec5032af4f3
Grigory Grebnev <antonianogroboracevskij@gmail.com>
2023-02-27 17:55:05 +0300

    added Second_Home_work

---
+++ Second_Home_work/Home_work013/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondDigit = SecondDigit(number);
+Console.WriteLine(number > 100? $"Третья цифра числа {number} -> {secondDigit}":
+ $"Число {number} не является трёхзначным");
+
+int SecondDigit(int numb)
+{
+   if (numb < 100) return 0;
+   if (numb > 100 && numb < 1000) return numb % 10;
+   if ( numb >= 1000)
+   {
+      while (numb >= 1000)
+      {
+        numb = numb / 10;
+      }
+   }  return numb % 10; 
+}
---
+++ Second_Home_work/Home_work015/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая 
+//принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string dayOfWeek = DayOfWeek(number);
+
+Console.WriteLine(number < 6 && number > 0 ? $"Число {number} {dayOfWeek} день" :
+$"Число {number} {dayOfWeek}" , $"Число {number} вообще не является днём недели");
+
+string DayOfWeek(int numb)
+{
+    if (numb < 6 && numb > 0) return "не выходной";
+    else if (numb <= 0 || numb > 7) return "Такого дня нет";
+    else
+    return "Выходной";
+}

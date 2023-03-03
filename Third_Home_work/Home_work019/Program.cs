68caaee58288b86ea692a4e639618d5b10579b3d
Grigory Grebnev <antonianogroboracevskij@gmail.com>
2023-03-03 15:26:49 +0300

    added Third Home_work

---
+++ Third_Home_work/Home_work019/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите число: ");
+string fiveDigitNumber = Console.ReadLine();
+
+void Palindrom(string number)
+{
+    if (number.Length == 5)
+    {
+        int i = number[0]; int j = number[number.Length - 1];
+        int g = number[1]; int l = number[number.Length - 2];
+        if (i == j && g == l) Console.WriteLine($"Число {number} является палиндром");
+        else Console.WriteLine($"Число {number} не является палиндром");
+    }
+    else Console.WriteLine("Это не пятизначное число!");
+}
+
+Palindrom(fiveDigitNumber);
---
+++ Third_Home_work/Home_work021/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход 
+// координаты двух точек и находит расстояние
+//  между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты точек А");
+
+Console.WriteLine("Введите точки по X:");
+int xCoordA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точки по Y:");
+int yCoordA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точки по Z:");
+int zCoordA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точек B");
+
+Console.WriteLine("Введите точки по X:");
+int xCoordB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точки по Y:");
+int yCoordB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точки по Z:");
+int zCoordB = Convert.ToInt32(Console.ReadLine());
+
+double distance = FindAB(xCoordA,yCoordA,zCoordA,xCoordB,yCoordB,zCoordB);
+Console.WriteLine($"Расстояние между точками равно:{Math.Round(distance)}");
+
+double FindAB(int xA, int yA, int xB, int yB, int zA, int zB)
+{
+    return Math.Sqrt(Math.Pow(xA-xB, 2)+Math.Pow(yA-yB, 2)+Math.Pow(zA-zB, 2));
+} 

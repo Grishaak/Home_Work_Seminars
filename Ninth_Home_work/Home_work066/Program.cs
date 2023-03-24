d9c6a8234e63ed6bfac156f4ef7fd3d22e602e0b
Grigory Grebnev <antonianogroboracevskij@gmail.com>
2023-03-24 21:20:27 +0300

    added Ninth Home_work

---
+++ Ninth_Home_work/Home_work066/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N.
+// Напишите программу, которая найдёт
+// сумму натуральных элементов в промежутке от M до N.
+// Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите натуральное M:");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное N:");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+int naturalSum = NaturalSum(numberOne, numberTwo);
+System.Console.WriteLine($"{naturalSum}");
+
+int NaturalSum(int numOne, int numTwo)
+{
+    if (numOne <=  numTwo) return numOne + NaturalSum(numOne + 1, numTwo);
+    else return 0;
+}
---
+++ Ninth_Home_work/Home_work068/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 68: Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите m:");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n:");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+int acr = AchermanFunction(numberOne, numberTwo);
+Console.WriteLine($"m = {numberOne}, n = {numberTwo} -> A(m,n) = {acr}");
+
+int AchermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return AchermanFunction(m - 1, 1);
+    }
+    else
+    {
+        return AchermanFunction(m - 1, AchermanFunction(m, n - 1));
+    }
+}
